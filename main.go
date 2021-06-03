d6baa2c7e08bc420285f9d22634a4995e5227519
catmandude <ozziemiller555@gmail.com>
2021-06-02 22:24:11 -0500

    new handlers

--- main.go
+++ main.go
@@ -4,14 +4,17 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"github.com/catmandude/nic-jackson-go/handlers"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
+	gh := handlers.Goodbye(l)
 
 	sm := http.NewServeMux() //points to the endpoint /thing
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
 
 	http.ListenAndServe(":9090", sm) //specify our servemux
 }
