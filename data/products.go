e9b7bf14e40e9e2b1b0f307f9b772affc352553f
catmandude <ozziemiller555@gmail.com>
2021-06-03 17:57:34 -0500

    handlers and data

--- data/products.go
+++ data/products.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
 
 type Product struct {
 	ID int `json:"id"`
@@ -12,7 +16,14 @@ type Product struct {
 	UpdatedOn string `json:"-"`
 }
 
-func GetProducts() []*Product {
+type Products []*Product
+
+func (p *Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+func GetProducts() Products {
 	return productList
 }
 
