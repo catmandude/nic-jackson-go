d7d665615d0414ac7163ad6d73bfcb8ee8ca82fa
catmandude <ozziemiller555@gmail.com>
2021-06-02 22:26:49 -0500

    corrected handlers

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
-	gh := handlers.Goodbye(l)
+	gh := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux() //points to the endpoint /thing
 	sm.Handle("/", hh)
